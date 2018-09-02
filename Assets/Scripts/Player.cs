deeef7a7f0a9b5edc652dc90f46b1fc69cdb8dd2
OneStig <therealonestig@gmail.com>
2018-09-02 10:25:45 -0700

    Added player

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour {
+
+    private Rigidbody playerRigidbody;
+
+
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
