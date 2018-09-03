0ae45f3a32cbfc56c49a8de7345ce13d873cf009
OneStig <therealonestig@gmail.com>
2018-09-03 11:34:04 -0700

    Flipping player on change of direction

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -5,6 +5,9 @@ using UnityEngine;
 public class Player : MonoBehaviour {
 
     private Rigidbody playerRigidbody;
+    private bool facingRight;
+    private float horizontal;
+    private float vertical;
 
     public float moveSpeed = 0.2f; //to be tuned in unity later
     public float[] clampPos = {
@@ -17,26 +20,50 @@ public class Player : MonoBehaviour {
 
 	void Start () {
         playerRigidbody = GetComponent<Rigidbody>();
+        facingRight = true;
 	}
 	
 	void Update () {
+        horizontal = Input.GetAxisRaw("Horizontal");
+        vertical = Input.GetAxisRaw("Vertical");
+
         updateLocation();
-	}
+
+        updateScale();
+    }
 
     private void updateLocation()
     {
-        float horizontal = Input.GetAxisRaw("Horizontal");
-        float vertical = Input.GetAxisRaw("Vertical");
-
         //Debug.Log("Horizontal axis: " + horizontal.ToString() + " Vertical axis: " + vertical.ToString());
 
-        float newX = Mathf.Clamp(transform.position.x + (moveSpeed * horizontal), clampPos[0], clampPos[1]);
-        float newZ = Mathf.Clamp(transform.position.z + (moveSpeed * vertical), clampPos[2], clampPos[3]);
+        float newX = Mathf.Clamp(transform.position.x + (moveSpeed * horizontal * Time.deltaTime),
+                                 clampPos[0],
+                                 clampPos[1]);
 
-        Vector3 newPos = new Vector3(newX,
-                                     transform.position.y, //locked by rigidbody
-                                     newZ);
+        float newZ = Mathf.Clamp(transform.position.z + (moveSpeed * vertical * Time.deltaTime),
+                                 clampPos[2],
+                                 clampPos[3]);
+
+        Vector3 newPos = new Vector3(newX, transform.position.y, newZ);
 
         playerRigidbody.position = newPos;
+    }
+
+    private void updateScale()
+    {
+        if (horizontal < 0)
+        {
+            facingRight = false;
+            transform.localScale = new Vector3(Mathf.Abs(transform.localScale.x) * -1,
+                                               transform.localScale.y,
+                                               transform.localScale.z);
+        }
+        else if (horizontal > 0)
+        {
+            facingRight = true;
+            transform.localScale = new Vector3(Mathf.Abs(transform.localScale.x),
+                                               transform.localScale.y,
+                                               transform.localScale.z);
+        }
     }
 }
